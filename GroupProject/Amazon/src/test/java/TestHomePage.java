2e997a097b68a4bc6cdaf5a7d2f794fb946c29f1
mdhossain62 <mhossain456@gmail.com>
2017-01-15 11:54:04 -0500

    first commit

---
+++ GroupProject/Amazon/src/test/java/TestHomePage.java
@@ -0,0 +1,11 @@
+import base.CommonAPI;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+/**
+ * Created by DAGR8DON on 1/4/17.
+ */
+public class TestHomePage extends CommonAPI {
+
+}
---
+++ GroupProject/CNN/src/test/java/HeadLines.java
@@ -0,0 +1,10 @@
+/**
+ * Created by JHANGIR on 1/5/2017.
+ */
+public class HeadLines {
+
+    public void searchNews() throws InterruptedException{
+
+    }
+
+}
